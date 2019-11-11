c6f9e8e110a10f262b0f38ff62c1108a99387345
Mike Cramer <mcramer@austin.utexas.edu>
2019-11-11 14:09:02 -0600

    "murder your darlings" or being just as fast and being technically more correct.
    fold list is stored as a single dimension array rather than two, allows for some fractal that may not be a superset of a previous generation.
    
    iteration has to be tracked as a property rather than a calculation.

--- swift/dragon_recursive.swift
+++ swift/dragon_recursive.swift
@@ -1,56 +1,56 @@
 #!/usr/bin/swift
 
-
 import Foundation
 // import UIKit
 
 struct PaperFractal {
-  let foldlist : [[Bool]]
-  var iteration : Int{ foldlist.count }
-  var flatFoldList : [Bool] { foldlist.reduce([], +) }
+  let foldList: [Bool]
+  let iteration: Int
 }
 
-let emptyPaperFractal  = PaperFractal(foldlist: [])
+let emptyPaperFractal  = PaperFractal(foldList: [], iteration: 0)
 typealias PaperFractalStep = (PaperFractal)->PaperFractal
 func dragonStep(_ pastState: PaperFractal)->PaperFractal{
-  return  PaperFractal(foldlist : ( [pastState.flatFoldList + [true] + pastState.flatFoldList.reversed().map{!$0}] ))
+  return  PaperFractal(
+    foldList: pastState.foldList + [true] + pastState.foldList.reversed().map{!$0},
+    iteration: pastState.iteration + 1
+  )
 }
 
 func iterateUpTo(previousState: PaperFractal, fractalStep: PaperFractalStep,  iteration requestedIteraton: Int)->PaperFractal{
   return  requestedIteraton <= previousState.iteration
     ? previousState
-    : fractalStep(
-        iterateUpTo(
-          previousState: previousState,
-          fractalStep: fractalStep,
-          iteration: requestedIteraton - 1)
-        )
+   : fractalStep(
+      iterateUpTo(
+        previousState: previousState,
+        fractalStep: fractalStep,
+        iteration: requestedIteraton - 1)
+  )
 }
 
-func turnNumber(previousState: PaperFractal, fractalStep: PaperFractalStep, requestedTurnNumber : Int)->Bool{
-  if requestedTurnNumber <= previousState.flatFoldList.count {
-    return previousState.flatFoldList[requestedTurnNumber - 1]
-  }
-  return turnNumber(previousState: fractalStep(previousState), fractalStep: fractalStep, requestedTurnNumber : requestedTurnNumber - 1)
+func turnNumber(previousState: PaperFractal, fractalStep: PaperFractalStep, requestedTurnNumber: Int)->Bool{
+  return requestedTurnNumber <= previousState.foldList.count
+    ? previousState.foldList[requestedTurnNumber - 1]
+   : turnNumber(previousState: fractalStep(previousState), fractalStep: fractalStep, requestedTurnNumber: requestedTurnNumber - 1)
 }
 
 let maxIteration = 32
 print("calculating fractal up to", maxIteration)
 let maxIteration_startTime = Date().timeIntervalSince1970
 let df = iterateUpTo(previousState: emptyPaperFractal, fractalStep: dragonStep, iteration: maxIteration )
-// print(df.flatFoldList)
+// print(df.foldList)
 print("Calculated in " + String(Date().timeIntervalSince1970 - maxIteration_startTime ))
 
 print("----")
-print("length of flatfoldlist")
-let flatfoldlist_startTime = Date().timeIntervalSince1970
-let lastTurnNumber = df.flatFoldList.count
+print("length of foldList")
+let foldList_startTime = Date().timeIntervalSince1970
+let lastTurnNumber = df.foldList.count
 print( " ", lastTurnNumber)
-print("Accessed in " + String(Date().timeIntervalSince1970 - flatfoldlist_startTime ))
+print("Accessed in " + String(Date().timeIntervalSince1970 - foldList_startTime ))
 
 print("----")
 print("turn number \(lastTurnNumber)")
 let turnNumber_startTime = Date().timeIntervalSince1970
-print("   is \( turnNumber(previousState: df, fractalStep: dragonStep, requestedTurnNumber : lastTurnNumber))")
+print("   is \( turnNumber(previousState: df, fractalStep: dragonStep, requestedTurnNumber: lastTurnNumber))")
 print("Accessed in " + String(Date().timeIntervalSince1970 - turnNumber_startTime ))
 
